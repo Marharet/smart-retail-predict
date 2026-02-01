3add57fbb26b16f48a8bb52199d23679a610c4d3
Marharet <margo1702prachuk@gmail.com>
2026-02-01 13:17:28 +0200

    Added basic project structure and database configuration

---
+++ db/db_config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
